39bfee480b3d84d1083475ae28629051c58650db
43h <cw.clx@outlook.com>
2025-07-21 16:08:07 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ REM 开发运行脚本 - ics-dp
 REM =====================================
 
 cd src
-go build -o ../ics-dp.exe main.go webshell.go csmp.go vncAddress.go
+go build -o ../ics-dp.exe main.go webshell.go csmp.go vncAddress.go vnc.go
 cd ..
